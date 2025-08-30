0464837b8d578ee815b9462e173dfd49c4af58c7
ayushjoshi45 <ayushjoshi3725@gmail.com>
2025-08-30 23:00:51 +0530

    Database + hooks + cmponents

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,7 +13,15 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as user from "../user.js";
+import type * as contacts from "../contacts.js";
+import type * as dashboard from "../dashboard.js";
+import type * as email from "../email.js";
+import type * as expenses from "../expenses.js";
+import type * as groups from "../groups.js";
+import type * as inngest from "../inngest.js";
+import type * as seed from "../seed.js";
+import type * as settlements from "../settlements.js";
+import type * as users from "../users.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -24,7 +32,15 @@ import type * as user from "../user.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  user: typeof user;
+  contacts: typeof contacts;
+  dashboard: typeof dashboard;
+  email: typeof email;
+  expenses: typeof expenses;
+  groups: typeof groups;
+  inngest: typeof inngest;
+  seed: typeof seed;
+  settlements: typeof settlements;
+  users: typeof users;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
